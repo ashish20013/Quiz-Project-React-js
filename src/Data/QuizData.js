c708b7fa3d3df67bf8fe63c56d1615bd31766e3c
Ashish Yadav <130742666+ashish20013@users.noreply.github.com>
2023-05-18 09:39:26 +0530

    Hii i am quize Project assiment

---
+++ src/Data/QuizData.js
@@ -0,0 +1,46 @@
+
+
+const QuizData = [
+    {
+        question: " whar is React",
+        option: [" langues", " compiler ", "librery ", "other"],
+        answer: 3,
+    }, {
+        question: " whar is javascript",
+        option: [" langues", " compiler ", "librery ", "other"],
+        answer: 1,
+    }, {
+        question: " whar is MUI",
+        option: [" component librer", " compiler ", "librery ", "other"],
+        answer: 1,
+    }, {
+        question: " whar is localStorege",
+        option: [" langues", " way to store data clint site ", "librery ", "other"],
+        answer: 3,
+    }, {
+        question: " how many types local storege",
+        option: [1, 2, 3, 4],
+        answer: 2,
+    }, {
+        question: " whar is routing",
+        option: [" langues", " compiler ", "way to redirect one page to another page ", "other"],
+        answer: 3,
+    }, {
+        question: " whar is your mentor name",
+        option: [" none of these", " rahul ", "jagdesh ", "subham"],
+        answer: 3,
+    }, {
+        question: " whar is css",
+        option: [" style", " languge ", "scripting langue ", "Markup lang."],
+        answer: 3,
+    }, {
+        question: " whar is your project",
+        option: [" none", " todo list project ", "Quize project ", "other"],
+        answer: 3,
+    }, {
+        question: " whar is Axios",
+        option: [" kuchh nahi", " fetch data based librery ", "librery ", "other"],
+        answer: 2,
+    },
+];
+export default QuizData;
